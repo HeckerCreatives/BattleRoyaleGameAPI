fb2ad74c9bfc2c5f63392a85995d70b08e7eb7a5
HeckerCreatives <creativebrainstudiosinc@gmail.com>
2025-08-02 12:05:03 +0800

    pooosh

--- controllers/leaderboard.js
+++ controllers/leaderboard.js
@@ -1,5 +1,6 @@
 const { default: mongoose } = require("mongoose");
 const Leaderboard = require("../models/Leaderboard");
+const Usergamedetails = require("../models/Usergamedetails")
 
 exports.getleaderboard = async (req, res) => {
     const {id, username} = req.user
@@ -64,4 +65,127 @@ exports.updateuserleaderboard = async (req, res) => {
     })
 
     return res.json({ message: "success" })
+}
+
+exports.getkillleaderboard = async (req, res) => {
+    const {id, username} = req.user
+
+    const lbdata = await Usergamedetails.find({kill: {$gt: 0}})
+    .populate({
+        path: "owner",
+        select: "username"
+    })
+    .limit(50)
+    .sort({kill: -1})
+    .then(data => data)
+    .catch(err => {
+        console.log(`There's a problem getting the leaderboard`)
+    })
+
+    if (lbdata.length <= 0){
+        return res.json({message: "success", data: {
+            leaderboard: {}
+        }})
+    }
+
+    let tempindex = 0;
+
+    const data = {
+        leaderboard: {}
+    }
+
+    lbdata.forEach(tempdata => {
+        const {owner, kill} = tempdata
+
+        data.leaderboard[tempindex] = {
+            user: owner.username,
+            amount: kill
+        };
+
+        tempindex++;
+    })
+
+    return res.json({message: "success", data: data})
+}
+
+exports.getdeathleaderboard = async (req, res) => {
+    const {id, username} = req.user
+
+    const lbdata = await Usergamedetails.find({death: {$gt: 0}})
+    .populate({
+        path: "owner",
+        select: "username"
+    })
+    .limit(50)
+    .sort({death: -1})
+    .then(data => data)
+    .catch(err => {
+        console.log(`There's a problem getting the leaderboard`)
+    })
+
+    if (lbdata.length <= 0){
+        return res.json({message: "success", data: {
+            leaderboard: {}
+        }})
+    }
+
+    let tempindex = 0;
+
+    const data = {
+        leaderboard: {}
+    }
+
+    lbdata.forEach(tempdata => {
+        const {owner, death} = tempdata
+
+        data.leaderboard[tempindex] = {
+            user: owner.username,
+            amount: death
+        };
+
+        tempindex++;
+    })
+
+    return res.json({message: "success", data: data})
+}
+
+exports.getlevelleaderboard = async (req, res) =>{
+    const {id, username} = req.user
+
+    const lbdata = await Usergamedetails.find()
+    .populate({
+        path: "owner",
+        select: "username"
+    })
+    .limit(50)
+    .sort({level: -1})
+    .then(data => data)
+    .catch(err => {
+        console.log(`There's a problem getting the leaderboard`)
+    })
+
+    if (lbdata.length <= 0){
+        return res.json({message: "success", data: {
+            leaderboard: {}
+        }})
+    }
+
+    let tempindex = 0;
+
+    const data = {
+        leaderboard: {}
+    }
+
+    lbdata.forEach(tempdata => {
+        const {owner, level} = tempdata
+
+        data.leaderboard[tempindex] = {
+            user: owner.username,
+            amount: level
+        };
+
+        tempindex++;
+    })
+
+    return res.json({message: "success", data: data})
 }
--- routes/leaderboard.js
+++ routes/leaderboard.js
@@ -1,9 +1,12 @@
 const router = require("express").Router()
-const { getleaderboard, updateuserleaderboard } = require("../controllers/leaderboard")
+const { getleaderboard, updateuserleaderboard, getkillleaderboard, getdeathleaderboard, getlevelleaderboard } = require("../controllers/leaderboard")
 const { protectplayer } = require("../middleware/middleware")
 
 router
     .get("/getleaderboard", protectplayer, getleaderboard)
+    .get("/getkillleaderboard", protectplayer, getkillleaderboard)
+    .get("/getdeathleaderboard", protectplayer, getdeathleaderboard)
+    .get("/getlevelleaderboard", protectplayer, getlevelleaderboard)
     .post("/updateuserleaderboard", protectplayer, updateuserleaderboard)
     
 module.exports = router;
