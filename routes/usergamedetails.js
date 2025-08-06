83299612978d5be1f088c6ed3407ea459f5869d9
HeckerCreatives <creativebrainstudiosinc@gmail.com>
2025-08-06 09:51:38 +0800

    Update usergamedetails.js

--- routes/usergamedetails.js
+++ routes/usergamedetails.js
@@ -1,10 +1,11 @@
 const router = require("express").Router()
-const { getusergamedetails, updateusergamedetails, checkingamemaintenance, useenergy, refundenergy } = require("../controllers/usergamedetails")
+const { getusergamedetails, updateusergamedetails, checkingamemaintenance, useenergy, refundenergy, getmatchhistory } = require("../controllers/usergamedetails")
 const { protectplayer } = require("../middleware/middleware")
 
 router
     .get("/getusergamedetails", protectplayer, getusergamedetails)
     .get("/checkingamemaintenance", protectplayer, checkingamemaintenance)
+    .get("/getmatchhistory", protectplayer, getmatchhistory)
     .post("/updateusergamedetails", protectplayer, updateusergamedetails)
     .post("/useenergy", protectplayer, useenergy)
     .post("/refundenergy", protectplayer, refundenergy)
