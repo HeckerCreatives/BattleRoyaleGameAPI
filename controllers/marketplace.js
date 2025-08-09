a14ba5084d78ac8124374141749cd6e0077b90b3
HeckerCreatives <creativebrainstudiosinc@gmail.com>
2025-08-09 09:33:37 +0800

    poooosh

---
+++ controllers/marketplace.js
@@ -0,0 +1,8 @@
+const Marketplace = require("../models/Marketplace")
+const Inventory = require("../models/Inventory")
+
+exports.buymarketplaceitem = async (req, res) => {
+    const {id, username} = req.user
+
+    
+}
