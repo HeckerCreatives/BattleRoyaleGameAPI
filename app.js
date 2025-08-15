c5314076bb448c92b28c5d2fc34fe07e514f4bd7
HeckerCreatives <daniel.janbiengabrielle@gmail.com>
2025-08-16 02:31:09 +0800

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const AWS = require('aws-sdk');
 const {gameserverinit} = require("./Initialize/init")
 
 const fleetIdsByRegion = {
-  'asia': 'fleet-92e24bef-c59d-4614-bed3-6d614ebebd8f'
+  'Asia': 'fleet-92e24bef-c59d-4614-bed3-6d614ebebd8f'
 };
 
 
